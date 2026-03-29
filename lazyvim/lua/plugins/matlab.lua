78aa4f5940d6e9a88b4a428a0e191d16661ae575
artemyev <a.e.artemyev@yandex.ru>
2026-03-29 12:35:37 +0700

    feat(lazyvim/plugins/matlab): add hotkeys + disable auto start

--- lazyvim/lua/plugins/matlab.lua
+++ lazyvim/lua/plugins/matlab.lua
@@ -3,7 +3,27 @@ return {
     "idossha/matlab.nvim",
     ft = "matlab",
     config = function()
-      require("matlab").setup()
+      require("matlab").setup({
+        auto_start = false,
+      })
     end,
+    keys = {
+      {
+        "<leader>ms",
+        function()
+          vim.cmd("MatlabStartServer")
+        end,
+        mode = "n",
+        desc = "MATLAB: Start server",
+      },
+      {
+        "<leader>mq",
+        function()
+          vim.cmd("MatlabStopServer")
+        end,
+        mode = "n",
+        desc = "MATLAB: Stop server",
+      },
+    },
   },
 }
