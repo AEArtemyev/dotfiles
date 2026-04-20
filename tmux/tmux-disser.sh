87c24ba603690d1141f51dd656449fbcb57968df
artemyev <a.e.artemyev@yandex.ru>
2026-04-20 09:42:37 +0700

    fix(tmux/tmux-disser.sh): переделал

--- tmux/tmux-disser.sh
+++ tmux/tmux-disser.sh
@@ -5,44 +5,55 @@ SESSION="disser"
 # Имя tmux-сессии, с которой будет работать скрипт
 
 tmux has-session -t "$SESSION" 2>/dev/null && exec tmux attach -t "$SESSION"
-# Проверяем, существует ли уже сессия с именем "work"
+# Проверяем, существует ли уже сессия с именем "disser"
 # 2>/dev/null скрывает сообщение об ошибке, если сессии нет
 # Если сессия существует, сразу заменяем текущий процесс на:
-# tmux attach -t work
+# tmux attach -t disser
 # То есть просто подключаемся к уже готовой сессии и дальше код ниже не выполняется
 
-tmux new-session -d -s "$SESSION" -n qm -c ~/plate_solver_project
+tmux new-session -d -s "$SESSION" -n vial -c ~/
 # Создаём новую tmux-сессию в фоне:
 # -d  => не подключаться сразу
 # -s  => имя сессии
-# -n  => имя первого окна: "qm"
-# -c  => стартовая директория для окна: ~/plate_solver_project
+# -n  => имя первого окна: "vial"
+# -c  => стартовая директория для окна: ~/
 
-tmux send-keys -t "$SESSION":qm 'lv' C-m
-# Отправляем в окно "qm" команду lv и Enter
+tmux send-keys -t "$SESSION":vial './Downloads/Vial-v0.7.5-x86_64.AppImage' C-m
+# Отправляем в окно "vial" команду и Enter
+
+tmux new-window -t "$SESSION" -n psj -c ~/work/disser/plate_solver_project
+# Создаём окно 2 внутри сессии "psj"
+# Имя окна: "psj"
+# Стартовая директория та же: ~/work/disser/plate_solver_project
+
+tmux send-keys -t "$SESSION":psj 'lv' C-m
+# Отправляем в окно "psj" команду lv и Enter
 # C-m эквивалентен нажатию клавиши Enter
 
-tmux new-window -t "$SESSION" -n qmMATLAB -c ~/QuadMesher/matlab/rsp
-# Создаём окно 3 внутри сессии "work"
-# Имя окна: "qmMATLAB"
-# Стартовая директория та же: ~/QuadMesher/matlab/rsp
+tmux new-window -t "$SESSION" -n psjMATLAB -c ~/work/disser/plate_solver_project
+# Создаём окно 3 внутри сессии "disser"
+# Имя окна: "psjMATLAB"
+# Стартовая директория та же: ~/work/disser/plate_solver_project
 
-tmux send-keys -t "$SESSION":qmMATLAB 'matlab -nodesktop' C-m
-# Отправляем в окно "qmMATLAB" команду запуска MATLAB
+tmux send-keys -t "$SESSION":psjMATLAB 'matlab -nodesktop' C-m
+# Отправляем в окно "psj" команду запуска MATLAB
 # C-m эквивалентен нажатию клавиши Enter
 
-tmux send-keys -t "$SESSION":qmMATLAB \
+tmux send-keys -t "$SESSION":psjMATLAB \
   "f = figure('Visible','off'); drawnow; close(f);" C-m
-# Отправляем в окно "qmMATLAB" команду открытия и закрытия figure
+# Отправляем в окно "psjMATLAB" команду открытия и закрытия figure
 # для инициализации графики
 
-tmux new-window -t "$SESSION" -n codex -c ~/plate_solver_project
-# Создаём второе окно внутри сессии "disser"
-# Имя окна: "codex"
-# Стартовая директория та же: ~/plate_solver_project
+tmux new-window -t "$SESSION" -n psjcodex -c ~/work/disser/plate_solver_project
+# Создаём окно 5 внутри сессии "disser"
+# Имя окна: "psjcodex"
+# Стартовая директория та же: ~/work/disser/plate_solver_project
 
-# tmux send-keys -t "$SESSION":codex 'codex' C-m
-# В окне "codex" запускаем команду codex
+# tmux send-keys -t "$SESSION":psjcodex 'codex' C-m
+# В окне "qmcodex" запускаем команду codex
+
+tmux select-window -t 1
+# Переключаемся на окно 1
 
 exec tmux attach -t "$SESSION"
 # В конце подключаемся к созданной сессии
